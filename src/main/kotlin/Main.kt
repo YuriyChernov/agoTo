5162799052d9c762519932cfd6fd629a36e0f726
Yuriy Chernov <Chernov.y.12@gmail.com>
2023-08-07 13:56:15 +0300

    Second Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val agoToSec = 216000
+    val agoToSec = 34000
     val sec = 60
     val hour = 3600
 
@@ -8,52 +8,29 @@ fun main() {
 
     val agoTo = when (agoToSec) {
         in 0..59 -> "был(а) только что"
-        in 60..3599 -> "был(а) $res1 минут назад"
-        in 3600..86399 -> "был(а) $res2 часов назад"
+        in 60..3599 -> "был(а) ${minToText(res1)} назад"
+        in 3600..86399 -> "был(а) ${hourToText(res2)} назад"
         in 86400..172800 -> "был(а) вчера"
         in 172801..259200 -> "был(а) позавчера"
         else -> "был(а) давно"
     }
-
-    if (res1 % 100 == 11) {
-        println("был(а) $res1 минут назад")
-    } else if (res1 % 100 == 12) {
-        println("был(а) $res1 минут назад")
-    } else if (res1 % 100 == 13) {
-        println("был(а) $res1 минут назад")
-    } else if (res1 % 100 == 14) {
-        println("был(а) $res1 минут назад")
-    } else if (res1 % 10 == 1) {
-        println("был(а) $res1 минуту назад")
-    } else if (res1 % 10 == 2) {
-        println("был(а) $res1 минуты назад")
-    } else if (res1 % 10 == 3) {
-        println("был(а) $res1 минуты назад")
-    } else if (res1 % 10 == 4) {
-        println("был(а) $res1 минуты назад")
-    } else println("был(а) $res1 минут назад")
-
-
-    if (res2 % 100 == 11) {
-        println("был(а) $res2 часов назад")
-    } else if (res2 % 100 == 12) {
-        println("был(а) $res2 часов назад")
-    } else if (res2 % 100 == 13) {
-        println("был(а) $res2 часов назад")
-    } else if (res2 % 100 == 14) {
-        println("был(а) $res2 часов назад")
-    } else if (res2 % 10 == 1) {
-        println("был(а) $res2 час назад")
-    } else if (res2 % 10 == 2) {
-        println("был(а) $res2 часа назад")
-    } else if (res2 % 10 == 3) {
-        println("был(а) $res2 часа назад")
-    } else if (res2 % 10 == 4) {
-        println("был(а) $res2 часа назад")
-    } else println("был(а) $res2 часов назад")
-
-    println("$agoTo")
+    println(agoTo)
 }
+
+fun minToText(min: Int) = when {
+    min in 11..14 -> "$min минут"
+    min % 10 == 1 -> "$min минуту"
+    min % 10 in 2..4 -> "$min минуты"
+    else -> "$min минут"
+}
+
+fun hourToText(hour: Int) = when {
+    hour in 11..14 -> "$hour часов"
+    hour % 10 == 1 -> "$hour час"
+    hour % 10 in 2..4 -> "$hour часа"
+    else -> "$hour часов"
+}
+
 
 
 
